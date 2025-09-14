d6a60ec54234bc62a03b4a9219d073aece6e1f7b
Vinay Dangodra <vinaydangodra28@gmail.com>
2025-09-14 05:43:18 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          animations: ['gsap'],
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    host: true
-  },
-  preview: {
-    port: 3000,
-    host: true
-  }
 })
